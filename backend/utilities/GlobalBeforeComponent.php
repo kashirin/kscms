0890363996ef436e4bc0acc9c013e8548dabb58a
Sergey Kashirin <kasser2005@rambler.ru>
2015-07-30 14:08:10 +0000

    remove vardump

--- backend/utilities/GlobalBeforeComponent.php
+++ backend/utilities/GlobalBeforeComponent.php
@@ -5,7 +5,14 @@ namespace backend\utilities;
 class GlobalBeforeComponent extends \yii\base\Component{
     
     public function init() {
-        var_dump('here before');
+        
+        
+        
+        
+        
+        
+        
+        ///////////////
         parent::init();
     }
     
