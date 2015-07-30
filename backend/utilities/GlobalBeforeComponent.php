623f280ed3d205f943ce5cbea85a3f0a0d54d836
Sergey Kashirin <kasser2005@rambler.ru>
2015-07-30 14:16:41 +0000

    register cookie name for screen_size

--- backend/utilities/GlobalBeforeComponent.php
+++ backend/utilities/GlobalBeforeComponent.php
@@ -7,13 +7,27 @@ class GlobalBeforeComponent extends \yii\base\Component{
     public function init() {
         
         
-        
-        
+       
+        $this->setDefaultScreenSizeInfo();
         
         
         
         ///////////////
         parent::init();
+    }
+    
+    protected function setDefaultScreenSizeInfo(){
+        
+        if( !Yii::$app->request->cookies->has('screen_width') ){
+            
+            Yii::$app->request->cookies->add(new \yii\web\Cookie([
+                'name' => 'screen_width',
+                'value' => false,
+            ]));
+
+            
+        }
+        
     }
     
 }
