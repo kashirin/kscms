7aa74e8181778d61d7fe9b712da136478783ed0f
Sergey Kashirin <kasser2005@rambler.ru>
2015-08-02 23:02:45 +0300

    add test mail script 2

--- common/config/main.php
+++ common/config/main.php
@@ -7,7 +7,15 @@ return [
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
-            'useFileTransport'=>'false'
+            'useFileTransport'=>false,
+             'transport' => [
+                                'class' => 'Swift_SmtpTransport',
+                                'host' => 'smtp.rambler.ru',
+                                'username' => 'kasser2005@rambler.ru',
+                                'password' => 'kascangel',
+                                'port' => '465',
+                                'encryption' => 'ssl',
+                                ],
         ],
     ],
 ];
