5622e311b559c121869fa4de73b9410384dbe933
Sergey Kashirin <kasser2005@rambler.ru>
2015-08-03 10:53:04 +0300

    change transport for mail

--- common/config/main.php
+++ common/config/main.php
@@ -7,15 +7,18 @@ return [
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
-            //'useFileTransport'=>false,
-             'transport' => [
+            'useFileTransport'=>false,
+             /*'transport' => [
                                 'class' => 'Swift_SmtpTransport',
                                 'host' => 'ssl://smtp.gmail.com',
                                 'username' => 'kasser2005@gmail.com',
                                 'password' => '0xmpfwtq',
                                 'port' => '587',
                                 'encryption' => 'tls',
-                                ],
+                                ],*/
+                'transport' => [
+                    'class' => 'Swift_MailTransport'
+                ]
         ],
     ],
 ];
