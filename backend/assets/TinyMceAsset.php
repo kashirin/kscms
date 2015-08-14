d76e95e10d99266294f63c52d28fdc74ba6a8cf5
Sergey Kashirin <kasser2005@gmail.com>
2015-08-14 18:42:57 +0300

    add frames for text

--- backend/assets/TinyMceAsset.php
+++ backend/assets/TinyMceAsset.php
@@ -13,6 +13,10 @@ class TinyMceAsset extends AssetBundle
     public $sourcePath = '@webroot/js/tinymce';
 	
 	public $jsOptions =['position'=>\yii\web\View::POS_HEAD ];
+	
+	public $publishOptions = [
+		'forceCopy' => true,
+	];
 
 	public $js = [
         'tinymce.min.js',
