2a8470f4180852e0236952dd9b9e36b2c16a43d4
Sergey Kashirin <kasser2005@rambler.ru>
2015-08-24 20:53:40 +0300

    hack for one image

--- frontend/widgets/views/carousel.php
+++ frontend/widgets/views/carousel.php
@@ -1,6 +1,6 @@
 
 <div id="banner" class="carousel">
-    <div id="carousel" class="carousel_inner">
+    <div id="carousel<? if(count($items)<=1){ echo '_one'; }?>" class="carousel_inner">
     	<?foreach($items as $item){?>
     		<div class="carousel_box"><a href="<?=$item->url?>"><img src="<?=$item->image?>"></a></div>
     	<?}?>
